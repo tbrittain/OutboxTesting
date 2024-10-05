e2fa830cfeb9e03e995167ab135428455f5e8020
tbrittain <treybrittain@gmail.com>
2024-10-05 17:50:08 -0500

    Add rabbitmq config

--- OutboxTesting.MassTransit/DependencyInjection.cs
+++ OutboxTesting.MassTransit/DependencyInjection.cs
@@ -25,9 +25,24 @@ public static class DependencyInjection
                 o.UseBusOutbox();
             });
 
-            // TODO: use rabbitmq with docker
-            x.UsingInMemory((context, configurator) =>
+            x.UsingRabbitMq((context, configurator) =>
             {
+                var rabbitMqSection = configuration.GetSection("RabbitMq");
+                var host = rabbitMqSection.GetValue<string>("Host");
+                Trace.Assert(!string.IsNullOrWhiteSpace(host), "Host is null or empty");
+
+                var username = rabbitMqSection.GetValue<string>("Username");
+                Trace.Assert(!string.IsNullOrWhiteSpace(username), "Username is null or empty");
+                
+                var password = rabbitMqSection.GetValue<string>("Password");
+                Trace.Assert(!string.IsNullOrWhiteSpace(password), "Password is null or empty");
+                
+                configurator.Host(host, "/", h =>
+                {
+                    h.Username(username);
+                    h.Password(password);
+                });
+
                 configurator.UseMessageRetry(r => r.Intervals(100, 500, 1000, 1500));
                 configurator.ConfigureEndpoints(context);
             });
